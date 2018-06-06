afbab5addd076f29ef9adb27c0f1be670d6a07d0
lu <82398795@qq.com>
2018-06-06 17:46:32 +0800

    init

---
+++ src/main/java/cn/waterlu/bookshop/ServiceRegistryApplication.java
@@ -0,0 +1,19 @@
+package cn.waterlu.bookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ *
+ * @author lu
+ * @date 2018/6/6
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+}
